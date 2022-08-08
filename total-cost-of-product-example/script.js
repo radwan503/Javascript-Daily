424861a20f82c6725d7c0e9774834bc5b546476a
Radwan Ahmed Anik <radwancse503@gmail.com>
2022-08-09 00:49:22 +0600

    add to cart count

--- total-cost-of-product-example/script.js
+++ total-cost-of-product-example/script.js
@@ -3,25 +3,25 @@ const products = [
     name: 'laptop',
     price: 50000,
     quantity: 1,
-    image: 'https://i.imgur.com/w2rCsEw.jpg',
+    image: 'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXBwbGUlMjBsYXB0b3B8ZW58MHx8MHx8&w=1000&q=80',
   },
   {
     name: 'shirt',
     price: 500,
     quantity: 8,
-    image: 'https://i.imgur.com/w2rCsEw.jpg',
+    image: 'https://www.collinsdictionary.com/images/full/tshirt_204029461_1000.jpg',
   },
   {
     name: 'phone',
     price: 20000,
     quantity: 1,
-    image: 'https://i.imgur.com/w2rCsEw.jpg',
+    image: 'https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGhvbmV8ZW58MHx8MHx8&w=1000&q=80',
   },
   {
     name: 'pant',
     price: 800,
     quantity: 2,
-    image: 'https://i.imgur.com/w2rCsEw.jpg',
+    image: 'https://5.imimg.com/data5/UV/UD/MY-52691782/mens-cotton-pant-500x500.jpg',
   },
 ];
 
@@ -35,18 +35,18 @@ for (const product of products) {
 
   let cardContent = `
   <div class="col">
-    <div class="card shadow-sm">
+    <div class="card shadow-sm" id="cardSingle">
       <img src="${product.image}" class="img-fluid product-image" alt="...">
       <div class="card-body">
-        <p class="card-text">${product.name}</p><small class="text-muted">${product.quantity} piece</small>
+        <p class="card-text fw-bold text-capitalize" id="card-text" >${product.name}</p><small class="text-muted">${product.quantity} </small>piece
         <p>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
         <div class="d-flex justify-content-between align-items-center">
           <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-secondary">Add to Cart</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary" id="addToCart">Add to Cart</button>
             <button type="button" class="btn btn-sm btn-outline-secondary">Details</button>
           </div>
 
-          <h1 class="card-title pricing-card-title">$ ${product.price}<small class="text-muted fw-light">/mo</small></h1><br>
+          <h1 class="card-title pricing-card-title">$<span id="price"> ${product.price}</span><small class="text-muted fw-light">/mo</small></h1><br>
           
         
         </div>
@@ -58,10 +58,74 @@ for (const product of products) {
   createSingleCard.innerHTML = cardContent;
 
   // total
-  totalInitial = totalInitial + product.price * product.quantity;
+  //totalInitial = totalInitial + product.price * product.quantity;
 }
+
+const btns = document.querySelectorAll('#addToCart');
+let totalCost = 0;
+for (const btn of btns) {
+  btn.addEventListener('click', function (e) {
+    const productQuantity = e.currentTarget.parentElement.parentElement.parentElement.children[1].innerText
+    console.log(productQuantity)
+    const priceValue = e.currentTarget.parentElement.parentElement.parentElement.children[3].children[1].children[0].innerText;
+    totalCost += priceValue * productQuantity;
+    e.currentTarget.style.backgroundColor = 'orange';
+    e.currentTarget.setAttribute('disabled', 'disabled')
+    totalCount.textContent = 'Total Amount »' + totalCost;
+  })
+}
+
 //total show in top
-totalCount.textContent = 'Total Amount »' + totalInitial;
+totalCount.textContent = 'Total Amount » ' + totalCost;
+
+
+// seach product
+let searchValue;
+const searchInput = document.querySelector('.product-search')
+const searchValueName = document.querySelector('.search-value-name');
+
+searchInput.addEventListener('keyup', function (e) {
+  if (e.which == 13) {
+    searchValue = searchInput.value;
+    searchProduct(products, searchValue)
+    searchInput.value = '';
+  }
+})
+
+
+function searchProduct(products, searchText) {
+  const matched = [];
+  for (const product of products) {
+    const name = product.name;
+    if (name.indexOf(searchText) != -1) {
+      matched.push(product.name)
+    }
+  }
+  searchValueName.innerHTML = matched;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // 2nd Example
@@ -92,28 +156,3 @@ totalCount.textContent = 'Total Amount »' + totalInitial;
 // card.innerHTML = displayMenu;
 
 
-
-// seach product
-let searchValue;
-const searchInput = document.querySelector('.product-search')
-const searchValueName = document.querySelector('.search-value-name');
-
-searchInput.addEventListener('keyup', function (e) {
-  if (e.which == 13) {
-    searchValue = searchInput.value;
-    searchProduct(products, searchValue)
-    searchInput.value = '';
-  }
-})
-
-
-function searchProduct(products, searchText) {
-  const matched = [];
-  for (const product of products) {
-    const name = product.name;
-    if (name.indexOf(searchText) != -1) {
-      matched.push(product.name)
-    }
-  }
-  searchValueName.innerHTML = matched;
-}
